63c68e48691efd1eb68b4f37a3cb8d169b4d56ba
manish30194 <manish30194@gmail.com>
2021-11-30 14:32:40 +0530

    added few more code for model

--- model.py
+++ model.py
@@ -1,1 +1,13 @@
 print("Adding all ML related model")
+
+
+def model1():
+    print("adding code of ML Model1")
+
+
+def model2():
+    print("adding code of ML Model1")
+
+
+def model3():
+    print("adding code of ML Model1")
